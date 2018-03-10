243db3028fc56d15ab358a45e27ecf6822a7a24e
Martin Matejčík <xmatej46@stud.fit.vutbr.cz>
2018-03-10 16:24:01 +0100

    Add helper method for observing events and make getViewModelFromProvider protected

--- mvvm/src/main/java/com/thefuntasty/mvvm/BaseBindingViewModelActivity.kt
+++ mvvm/src/main/java/com/thefuntasty/mvvm/BaseBindingViewModelActivity.kt
@@ -3,6 +3,7 @@ package com.thefuntasty.mvvm
 import android.arch.lifecycle.ViewModelProviders
 import android.databinding.ViewDataBinding
 import android.view.LayoutInflater
+import com.thefuntasty.mvvm.event.Event
 import com.thefuntasty.mvvm.factory.BaseViewModelFactory
 import dagger.android.support.DaggerAppCompatActivity
 import kotlin.reflect.KClass
@@ -17,7 +18,11 @@ abstract class BaseBindingViewModelActivity<VM : BaseViewModel, B : ViewDataBind
 
     abstract fun inflateBindingLayout(layoutInflater: LayoutInflater): B?
 
-    fun getViewModelFromProvider(factory: BaseViewModelFactory<VM>, viewModelKClass: KClass<VM>): VM {
+    protected fun getViewModelFromProvider(factory: BaseViewModelFactory<VM>, viewModelKClass: KClass<VM>): VM {
         return ViewModelProviders.of(this, factory).get(viewModelKClass.java)
+    }
+
+    protected fun <T : Event> observerEvent(event: KClass<T>, observer: (T) -> Unit) {
+        viewModel.observeEvent(this, event, observer)
     }
 }
