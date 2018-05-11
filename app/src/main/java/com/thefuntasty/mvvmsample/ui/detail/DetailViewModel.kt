065e9cd830239b75e972628f1c60e0154d31551e
Martin Matejčík <martin.matejcik@thefuntasty.com>
2018-05-11 15:50:07 +0200

    Try to fix build

--- app/src/main/java/com/thefuntasty/mvvmsample/ui/detail/DetailViewModel.kt
+++ app/src/main/java/com/thefuntasty/mvvmsample/ui/detail/DetailViewModel.kt
@@ -12,6 +12,6 @@ class DetailViewModel @Inject constructor() : BaseViewModel<DetailViewState>() {
     }
 
     fun incrementNumber() {
-        viewState.number.value = viewState.number.value() + 1
+        viewState.number.value = viewState.number.value + 1
     }
 }
