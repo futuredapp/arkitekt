80d8ba3fdf1b86f95f5623f0321d1936bb44800f
Martin Matejčík <martin.matejcik@thefuntasty.com>
2018-05-21 19:57:24 +0200

    Add map and switchMap extensions

--- mvvm/src/main/java/com/thefuntasty/mvvm/livedata/LiveDataExtensions.kt
+++ mvvm/src/main/java/com/thefuntasty/mvvm/livedata/LiveDataExtensions.kt
@@ -3,6 +3,7 @@ package com.thefuntasty.mvvm.livedata
 import android.arch.lifecycle.LifecycleOwner
 import android.arch.lifecycle.LiveData
 import android.arch.lifecycle.Observer
+import android.arch.lifecycle.Transformations
 
 fun <T> LiveData<T>.observe(lifecycleOwner: LifecycleOwner, callback: (T?) -> Unit) {
     observe(lifecycleOwner, Observer { callback(it) })
@@ -11,3 +12,9 @@ fun <T> LiveData<T>.observe(lifecycleOwner: LifecycleOwner, callback: (T?) -> Un
 fun <T> LiveData<T>.observeNonNull(lifecycleOwner: LifecycleOwner, callback: (T) -> Unit) {
     observe(lifecycleOwner, Observer { it?.let(callback) })
 }
+
+fun <T, R> LiveData<T>.map(func: (T) -> R): LiveData<R> =
+        Transformations.map(this, func)
+
+fun <T, R> LiveData<T>.switchMap(func: (T) -> LiveData<R>): LiveData<R> =
+        Transformations.switchMap(this, func)
