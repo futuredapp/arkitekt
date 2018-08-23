2499ac5d21569029e5fb36cf02456ca2fad805df
Jakub Voneš <jakub.vones@thefuntasty.com>
2018-08-23 23:43:05 +0200

    Remove unused imports

--- app/src/main/java/com/thefuntasty/mvvmsample/ui/main/MainViewModelFactory.kt
+++ app/src/main/java/com/thefuntasty/mvvmsample/ui/main/MainViewModelFactory.kt
@@ -1,7 +1,6 @@
 package com.thefuntasty.mvvmsample.ui.main
 
 import com.thefuntasty.mvvm.factory.BaseViewModelFactory
-import com.thefuntasty.mvvmsample.ui.main.MainViewModel
 import javax.inject.Inject
 import javax.inject.Provider
 
