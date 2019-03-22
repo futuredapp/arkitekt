67710551aa3acabca0357f021e39e88f23debb3d
Lukas Sztefek <lukas.sztefek@gmail.com>
2019-03-22 16:55:05 +0100

    Add comments to base interactors

--- interactors/src/main/java/com/thefuntasty/interactors/BaseCompletabler.kt
+++ interactors/src/main/java/com/thefuntasty/interactors/BaseCompletabler.kt
@@ -2,10 +2,34 @@ package com.thefuntasty.interactors
 
 import io.reactivex.Completable
 
+/**
+ * Base interactor which wraps [Completable]. Instance of this
+ * interactor can be simply executed in cooperation with
+ * [com.thefuntasty.interactors.disposables.DisposablesOwner] interface.
+ *
+ * Wrapped stream is subscribed on [io.reactivex.schedulers.Schedulers.io] and
+ * observed on [io.reactivex.android.schedulers.AndroidSchedulers.mainThread]
+ * by default. You may override these through [workScheduler] and
+ * [resultScheduler] respectively.
+ */
 abstract class BaseCompletabler : BaseInteractor<Unit>() {
 
+    /**
+     * Prepares whole wrapped [Completable] Rx stream. This method do not
+     * subscribes to the stream.
+     */
     protected abstract fun prepare(): Completable
 
+    /**
+     * Creates internal [Completable] Rx stream, applies requested work
+     * & result schedulers and exposes this stream as a [Completable]. Method
+     * is handy when you want to combine streams of multiple interactors.
+     * For example:
+     *
+     * interactor_A.stream().flatMap {
+     *     interactor_B.stream()
+     * }
+     */
     fun stream(): Completable = prepare().applySchedulers()
 
     private fun Completable.applySchedulers(): Completable {
--- interactors/src/main/java/com/thefuntasty/interactors/BaseFlowabler.kt
+++ interactors/src/main/java/com/thefuntasty/interactors/BaseFlowabler.kt
@@ -2,10 +2,34 @@ package com.thefuntasty.interactors
 
 import io.reactivex.Flowable
 
+/**
+ * Base interactor which wraps [Flowable]. Instance of this
+ * interactor can be simply executed in cooperation with
+ * [com.thefuntasty.interactors.disposables.DisposablesOwner] interface.
+ *
+ * Wrapped stream is subscribed on [io.reactivex.schedulers.Schedulers.io] and
+ * observed on [io.reactivex.android.schedulers.AndroidSchedulers.mainThread]
+ * by default. You may override these through [workScheduler] and
+ * [resultScheduler] respectively.
+ */
 abstract class BaseFlowabler<T : Any> : BaseInteractor<T>() {
 
+    /**
+     * Prepares whole wrapped [Flowable] Rx stream. This method do not
+     * subscribes to the stream.
+     */
     protected abstract fun prepare(): Flowable<T>
 
+    /**
+     * Creates internal [Flowable] Rx stream, applies requested work
+     * & result schedulers and exposes this stream as a [Flowable]. Method
+     * is handy when you want to combine streams of multiple interactors.
+     * For example:
+     *
+     * interactor_A.stream().flatMap {
+     *     interactor_B.stream()
+     * }
+     */
     fun stream(): Flowable<T> = prepare().applySchedulers()
 
     private fun Flowable<T>.applySchedulers(): Flowable<T> {
