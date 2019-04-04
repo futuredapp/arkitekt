d864b6bb4023f9c8dcefa821b9b15f5d0961b3a2
Lukas Sztefek <lukas.sztefek@gmail.com>
2019-04-04 16:17:35 +0200

    Add comments to DefaultValue*LiveData

--- mvvm/src/main/java/com/thefuntasty/mvvm/livedata/DefaultValueLiveData.kt
+++ mvvm/src/main/java/com/thefuntasty/mvvm/livedata/DefaultValueLiveData.kt
@@ -1,7 +1,11 @@
 package com.thefuntasty.mvvm.livedata
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 
+/**
+ * Extended [LiveData] class with default value. Default value can't be null.
+ */
 class DefaultValueLiveData<T : Any>(defaultValue: T) : MutableLiveData<T>() {
 
     init {
