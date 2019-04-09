b8e972ac5775270d731a6160dc91c6efb62affc8
Lukas Sztefek <lukas.sztefek@gmail.com>
2019-04-09 15:56:17 +0200

    Add missing dash in documentation

--- mvvm/src/main/java/com/thefuntasty/mvvm/event/Event.kt
+++ mvvm/src/main/java/com/thefuntasty/mvvm/event/Event.kt
@@ -6,7 +6,7 @@ import com.thefuntasty.mvvm.ViewState
 /**
  * One time event sent from ViewModel to Fragment/Activity via [BaseViewModel.sendEvent].
  * Event is tied directly to specific Fragment/Activity via its ViewState. Events are
- * delivered through LiveData based bus.
+ * delivered through LiveData-based bus.
  *
  * Event is guaranteed to be delivered just once even screen rotation or a similar
  * operation is in progress.
