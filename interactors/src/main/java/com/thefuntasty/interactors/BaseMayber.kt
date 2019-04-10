9c52775cefdb4bc11261092d9405e57c98a63a05
Ondrej Kalman <ondrej.kalman@thefuntasty.com>
2019-04-10 10:09:17 +0200

    Feat: added BaseMayber interactor

--- interactors/src/main/java/com/thefuntasty/interactors/BaseMayber.kt
+++ interactors/src/main/java/com/thefuntasty/interactors/BaseMayber.kt
@@ -2,10 +2,34 @@ package com.thefuntasty.interactors
 
 import io.reactivex.Maybe
 
+/**
+ * Base interactor which wraps [Maybe]. Instance of this
+ * interactor can be simply executed in cooperation with
+ * [com.thefuntasty.interactors.disposables.DisposablesOwner] interface.
+ *
+ * Wrapped stream is subscribed on [io.reactivex.schedulers.Schedulers.io] and
+ * observed on [io.reactivex.android.schedulers.AndroidSchedulers.mainThread]
+ * by default. You may override these through [workScheduler] and
+ * [resultScheduler] respectively.
+ */
 abstract class BaseMayber<T : Any> : BaseInteractor<T>() {
 
+    /**
+     * Prepares whole wrapped [Maybe] Rx stream. This method does not
+     * subscribe to the stream.
+     */
     protected abstract fun prepare(): Maybe<T>
 
+    /**
+     * Creates internal [Maybe] Rx stream, applies requested work
+     * & result schedulers and exposes this stream as a [Maybe]. This method
+     * is handy when you want to combine streams of multiple interactors.
+     * For example:
+     *
+     * interactor_A.stream().flatMap {
+     *     interactor_B.stream()
+     * }
+     */
     fun stream(): Maybe<T> = prepare().applySchedulers()
 
     private fun Maybe<T>.applySchedulers(): Maybe<T> {
