3af735d3eb8e50d40bb4ac387ce6f7a8dc020a0e
rudolfhladik <hladik.rudolf@gmail.com>
2019-04-29 12:03:34 +0200

    Lower minSdk to 18 because there is no need to have it 19

--- buildSrc/src/main/kotlin/ProjectSettings.kt
+++ buildSrc/src/main/kotlin/ProjectSettings.kt
@@ -2,5 +2,5 @@ object ProjectSettings {
     const val applicationId = "com.thefuntasty.mvvmsample"
     const val compileSdk = 28
     const val targetSdk = 28
-    const val minSdk = 19
+    const val minSdk = 18
 }
