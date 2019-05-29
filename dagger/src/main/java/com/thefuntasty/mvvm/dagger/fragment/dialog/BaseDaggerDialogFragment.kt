66ad0b54ed865247eeb6bd821310b06ddc08a0e7
Ondrej Kalman <ondrej.kalman@thefuntasty.com>
2019-05-29 10:18:55 +0200

    Feat: added DialogFragment implementation

---
+++ dagger/src/main/java/com/thefuntasty/mvvm/dagger/fragment/dialog/BaseDaggerDialogFragment.kt
@@ -0,0 +1,26 @@
+package com.thefuntasty.mvvm.dagger.fragment.dialog
+
+import android.content.Context
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+import com.thefuntasty.mvvm.BaseViewModel
+import com.thefuntasty.mvvm.ViewState
+import com.thefuntasty.mvvm.fragment.dialog.ViewModelDialogFragment
+import dagger.android.AndroidInjector
+import dagger.android.DispatchingAndroidInjector
+import dagger.android.support.AndroidSupportInjection
+import dagger.android.support.HasSupportFragmentInjector
+import javax.inject.Inject
+
+abstract class BaseDaggerDialogFragment<VM : BaseViewModel<VS>, VS : ViewState, B : ViewDataBinding> :
+    ViewModelDialogFragment<VM, VS>(), HasSupportFragmentInjector {
+
+    @Inject internal lateinit var supportFragmentInjector: DispatchingAndroidInjector<Fragment>
+
+    override fun onAttach(context: Context) {
+        AndroidSupportInjection.inject(this)
+        super.onAttach(context)
+    }
+
+    override fun supportFragmentInjector(): AndroidInjector<Fragment> = supportFragmentInjector
+}
---
+++ mvvm/src/main/java/com/thefuntasty/mvvm/fragment/dialog/ViewModelDialogFragment.kt
@@ -0,0 +1,67 @@
+package com.thefuntasty.mvvm.fragment.dialog
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.ViewModelProviders
+import com.thefuntasty.mvvm.BaseViewModel
+import com.thefuntasty.mvvm.ViewModelCreator
+import com.thefuntasty.mvvm.ViewState
+import com.thefuntasty.mvvm.event.Event
+import kotlin.reflect.KClass
+
+/**
+ * Base DialogFragment class with built-in ViewModel support
+ */
+abstract class ViewModelDialogFragment<VM : BaseViewModel<VS>, VS : ViewState> : DialogFragment(),
+    ViewModelCreator<VM> {
+
+    /**
+     * Property which holds reference to layout identifier eg. R.layout.fragment_custom_bottomsheet.
+     * You should override this in your specific BottomSheetDialogFragment implementation.
+     */
+    abstract val layoutResId: Int
+
+    /**
+     * Property which holds BottomSheetDialogFragment tag
+     * You should override this in your specific BottomSheetDialogFragment implementation.
+     */
+    abstract val fragmentTag: String
+
+    /**
+     * Reference to Fragment ViewModel
+     */
+    val viewModel: VM by lazy {
+        getVM().apply {
+            lifecycle.addObserver(this)
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View =
+        inflater.inflate(layoutResId, container, false)
+
+    fun show(fragmentManager: FragmentManager) = show(fragmentManager, fragmentTag)
+
+    private fun getVM(): VM = ViewModelProviders.of(this, viewModelFactory).get(viewModelFactory.viewModelClass.java)
+
+    /**
+     * Get reference to Activity ViewModel. Make sure correct VM class is specified.
+     */
+    inline fun <reified AVM : BaseViewModel<*>> getActivityViewModel(): AVM =
+        ViewModelProviders.of(requireActivity()).get(AVM::class.java)
+
+    /**
+     * Observe event defined by event class and run observer lambda whenever event is
+     * received. This event class must be associated with current BottomSheetDialogFragment ViewState.
+     * @param event Observed event class
+     * @param observer Lambda called whenever event is received
+     */
+    @Suppress("UNCHECKED_CAST")
+    fun <EVENT : Event<VS>> observeEvent(event: KClass<out EVENT>, observer: (EVENT) -> Unit) {
+        viewModel.observeEvent(this, event, observer as (Event<VS>) -> Unit)
+    }
+}
+
