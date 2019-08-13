c2b4c8e3940c68363f6978bb844bbd8aeb2b0dac
Martin Matejčík <martin.matejcik@thefuntasty.com>
2019-08-13 20:45:26 +0200

    Update libraries
    Dagger removes HasFragmentInjector - base activities, fragments was updated

--- dagger/src/main/java/com/thefuntasty/mvvm/dagger/activity/BaseDaggerBindingActivity.kt
+++ dagger/src/main/java/com/thefuntasty/mvvm/dagger/activity/BaseDaggerBindingActivity.kt
@@ -2,25 +2,16 @@ package com.thefuntasty.mvvm.dagger.activity
 
 import android.os.Bundle
 import androidx.databinding.ViewDataBinding
-import androidx.fragment.app.Fragment
 import com.thefuntasty.mvvm.BaseViewModel
-import com.thefuntasty.mvvm.activity.BindingViewModelActivity
 import com.thefuntasty.mvvm.ViewState
+import com.thefuntasty.mvvm.activity.BindingViewModelActivity
 import dagger.android.AndroidInjection
-import dagger.android.AndroidInjector
-import dagger.android.DispatchingAndroidInjector
-import dagger.android.support.HasSupportFragmentInjector
-import javax.inject.Inject
 
 abstract class BaseDaggerBindingActivity<VM : BaseViewModel<VS>, VS : ViewState, B : ViewDataBinding> :
-    BindingViewModelActivity<VM, VS, B>(), HasSupportFragmentInjector {
-
-    @Inject internal lateinit var supportFragmentInjector: DispatchingAndroidInjector<Fragment>
+    BindingViewModelActivity<VM, VS, B>() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
     }
-
-    override fun supportFragmentInjector(): AndroidInjector<Fragment> = supportFragmentInjector
 }
--- dagger/src/main/java/com/thefuntasty/mvvm/dagger/fragment/BaseDaggerFragment.kt
+++ dagger/src/main/java/com/thefuntasty/mvvm/dagger/fragment/BaseDaggerFragment.kt
@@ -1,25 +1,16 @@
 package com.thefuntasty.mvvm.dagger.fragment
 
 import android.content.Context
-import androidx.fragment.app.Fragment
 import com.thefuntasty.mvvm.BaseViewModel
-import com.thefuntasty.mvvm.fragment.ViewModelFragment
 import com.thefuntasty.mvvm.ViewState
-import dagger.android.AndroidInjector
-import dagger.android.DispatchingAndroidInjector
+import com.thefuntasty.mvvm.fragment.ViewModelFragment
 import dagger.android.support.AndroidSupportInjection
-import dagger.android.support.HasSupportFragmentInjector
-import javax.inject.Inject
 
 abstract class BaseDaggerFragment<VM : BaseViewModel<VS>, VS : ViewState> :
-    ViewModelFragment<VM, VS>(), HasSupportFragmentInjector {
-
-    @Inject internal lateinit var supportFragmentInjector: DispatchingAndroidInjector<Fragment>
+    ViewModelFragment<VM, VS>() {
 
     override fun onAttach(context: Context) {
         AndroidSupportInjection.inject(this)
         super.onAttach(context)
     }
-
-    override fun supportFragmentInjector(): AndroidInjector<Fragment> = supportFragmentInjector
 }
