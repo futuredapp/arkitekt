f48a69c74729269ec206bd638d49c0b5065df59f
matsem <matej.semancik@gmail.com>
2019-10-15 10:32:25 +0200

    Add misspell issue detection, refactor to use regex

--- mvvm-lint/src/main/java/com/thefuntasty/lint/WrongEventNameDetector.kt
+++ mvvm-lint/src/main/java/com/thefuntasty/lint/WrongEventNameDetector.kt
@@ -10,12 +10,13 @@ import com.android.tools.lint.detector.api.Scope
 import com.android.tools.lint.detector.api.Severity
 import org.jetbrains.uast.UClass
 import java.util.EnumSet
+import java.util.regex.Pattern
 
 class WrongEventNameDetector : Detector(), Detector.UastScanner {
 
     companion object {
-        val ISSUE = Issue.create(
-            id = "MvvmWrongEventName",
+        val ISSUE_SUFFIX = Issue.create(
+            id = "MvvmEventNameMissingSuffix",
             briefDescription = "Wrong event name",
             explanation = "Event names should end with 'Event' suffix",
             category = Category.CORRECTNESS,
@@ -26,6 +27,22 @@ class WrongEventNameDetector : Detector(), Detector.UastScanner {
                 EnumSet.of(Scope.JAVA_FILE, Scope.TEST_SOURCES)
             )
         )
+
+        val ISSUE_MISSPELL = Issue.create(
+            id = "MvvmEventNameMisspell",
+            briefDescription = "Misspelled event name",
+            explanation = "Event name is misspelled",
+            category = Category.CORRECTNESS,
+            priority = 5,
+            severity = Severity.WARNING,
+            implementation = Implementation(
+                WrongEventNameDetector::class.java,
+                EnumSet.of(Scope.JAVA_FILE, Scope.TEST_SOURCES)
+            )
+        )
+
+        private val PATTERN_MISSPELL = Pattern.compile("Event[a-z]+")
+        private val PATTERN_SUFFIX = Pattern.compile("(.+Event)")
     }
 
     override fun getApplicableUastTypes() = listOf(UClass::class.java)
@@ -35,29 +52,50 @@ class WrongEventNameDetector : Detector(), Detector.UastScanner {
     override fun visitClass(context: JavaContext, declaration: UClass) {
         super.visitClass(context, declaration)
 
-        declaration.superClass?.let { superClass ->
-            if (context.evaluator.getQualifiedName(superClass) != "com.thefuntasty.mvvm.event.Event") {
-                if (declaration.name?.endsWith("Event") == false) {
+        val className = declaration.name
+
+        val isMvvmEvent = context.evaluator.getQualifiedName(declaration) == "com.thefuntasty.mvvm.event.Event"
+        val extendsMvvmEvent = declaration.superClass?.let { context.evaluator.getQualifiedName(it) } == "com.thefuntasty.mvvm.event.Event"
+
+        val isEligibleForDetection = isMvvmEvent.not() && extendsMvvmEvent.not()
+
+
+        if (className != null && isEligibleForDetection) {
+            when {
+                PATTERN_MISSPELL.matcher(className).find() -> {
+                    val suggestedName = PATTERN_SUFFIX.matcher(className).let {
+                        it.find()
+                        it.group(1)
+                    }
+
                     context.report(
-                        issue = ISSUE,
+                        issue = ISSUE_MISSPELL,
                         scopeClass = declaration,
                         location = context.getNameLocation(declaration),
-                        message = "Event names should end with 'Event' suffix. Suggested name: ${declaration.name}Event",
-                        quickfixData = createQuickFix(declaration)
+                        message = "Event name is misspelled. Suggested name: $suggestedName",
+                        quickfixData = createQuickFix(className, suggestedName)
+                    )
+                }
+
+                PATTERN_SUFFIX.matcher(className).find().not() -> {
+                    val suggestedName = "${declaration.name}Event"
+
+                    context.report(
+                        issue = ISSUE_SUFFIX,
+                        scopeClass = declaration,
+                        location = context.getNameLocation(declaration),
+                        message = "Event names should end with 'Event' suffix. Suggested name: $suggestedName",
+                        quickfixData = createQuickFix(className, suggestedName)
                     )
                 }
             }
         }
     }
 
-    private fun createQuickFix(declaration: UClass): LintFix? {
-        return declaration.name?.let { name ->
-            LintFix.create()
-                .name("Replace with ${name}Event")
-                .replace()
-                .text(name)
-                .with("${name}Event")
-                .build()
-        }
-    }
+    private fun createQuickFix(declarationName: String, replacement: String) = LintFix.create()
+        .name("Replace with $replacement")
+        .replace()
+        .text(declarationName)
+        .with(replacement)
+        .build()
 }
