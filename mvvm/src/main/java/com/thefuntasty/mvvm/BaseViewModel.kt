33c7e93003b253499735f77baed084d2d6de0ca8
Martin Matejčík <martin.matejcik@thefuntasty.com>
2019-10-16 18:35:51 +0200

    Remove Observable implementation of databinding v1, use LiveData for ui binding instead.

--- mvvm/src/main/java/com/thefuntasty/mvvm/BaseViewModel.kt
+++ mvvm/src/main/java/com/thefuntasty/mvvm/BaseViewModel.kt
@@ -1,8 +1,6 @@
 package com.thefuntasty.mvvm
 
 import androidx.annotation.CallSuper
-import androidx.databinding.Observable
-import androidx.databinding.PropertyChangeRegistry
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleObserver
 import androidx.lifecycle.LifecycleOwner
@@ -22,11 +20,9 @@ import kotlin.reflect.KClass
  * no longer used and will be destroyed. Beside that, it handles one-shot [Event]s
  * send from ViewModel to Activity/Fragment.
  */
-abstract class BaseViewModel<VS : ViewState> : ViewModel(), Observable, LifecycleObserver {
+abstract class BaseViewModel<VS : ViewState> : ViewModel(), LifecycleObserver {
 
     abstract val viewState: VS
-
-    @Transient private var callbacks: PropertyChangeRegistry? = null
 
     private var onStartCalled = false
     private val liveEventBus = LiveEventBus<VS>()
@@ -114,53 +110,5 @@ abstract class BaseViewModel<VS : ViewState> : ViewModel(), Observable, Lifecycl
     @CallSuper
     override fun onCleared() {
         removeObservers()
-    }
-
-    // ----- Observable implementation -----
-
-    override fun addOnPropertyChangedCallback(callback: Observable.OnPropertyChangedCallback) {
-        synchronized(this) {
-            if (callbacks == null) {
-                callbacks = PropertyChangeRegistry()
-            }
-        }
-        callbacks?.add(callback)
-    }
-
-    override fun removeOnPropertyChangedCallback(callback: Observable.OnPropertyChangedCallback) {
-        synchronized(this) {
-            if (callbacks == null) {
-                return
-            }
-        }
-        callbacks?.remove(callback)
-    }
-
-    /**
-     * Notifies listeners that all properties of this instance have changed.
-     */
-    fun notifyChange() {
-        synchronized(this) {
-            if (callbacks == null) {
-                return
-            }
-        }
-        callbacks?.notifyCallbacks(this, 0, null)
-    }
-
-    /**
-     * Notifies listeners that a specific property has changed. The getter for the property
-     * that changes should be marked with [@Bindable] to generate a field in
-     * `BR` to be used as `fieldId`.
-     *
-     * @param fieldId The generated BR id for the Bindable field.
-     */
-    fun notifyPropertyChanged(fieldId: Int) {
-        synchronized(this) {
-            if (callbacks == null) {
-                return
-            }
-        }
-        callbacks?.notifyCallbacks(this, fieldId, null)
     }
 }
