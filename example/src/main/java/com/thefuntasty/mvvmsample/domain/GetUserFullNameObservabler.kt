abd629572327cbabcfd5483dec148fd3bc6b5ac2
Lukas -skywall- Sztefek <lukas.sztefek@thefuntasty.com>
2019-10-24 18:06:23 +0200

    Fix example interactors

--- example/src/main/java/com/thefuntasty/mvvmsample/domain/GetUserFullNameObservabler.kt
+++ example/src/main/java/com/thefuntasty/mvvmsample/domain/GetUserFullNameObservabler.kt
@@ -1,16 +1,15 @@
 package com.thefuntasty.mvvmsample.domain
 
-import com.thefuntasty.interactors.BaseObservabler
+import com.thefuntasty.interactors.interactors.BaseObservabler
 import com.thefuntasty.mvvmsample.data.store.UserStore
 import io.reactivex.Observable
 import javax.inject.Inject
 
 class GetUserFullNameObservabler @Inject constructor(
     private val userStore: UserStore
-) : BaseObservabler<String>() {
+) : BaseObservabler<Unit, String>() {
 
-    override fun prepare(): Observable<String> {
-        return userStore.getUser()
-            .map { "${it.firstName} ${it.lastName}" }
+    override fun prepare(args: Unit): Observable<String> {
+        return userStore.getUser().map { "${it.firstName} ${it.lastName}" }
     }
 }
--- example/src/main/java/com/thefuntasty/mvvmsample/domain/StateInteractor.kt
+++ example/src/main/java/com/thefuntasty/mvvmsample/domain/StateInteractor.kt
@@ -1,23 +1,17 @@
 package com.thefuntasty.mvvmsample.domain
 
-import com.thefuntasty.interactors.BaseMayber
+import com.thefuntasty.interactors.interactors.BaseMayber
 import io.reactivex.Maybe
 import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
-class StateInteractor @Inject constructor() : BaseMayber<Boolean>() {
-
-    private var emitSuccess: Boolean = false
+class StateInteractor @Inject constructor() : BaseMayber<Boolean, Boolean>() {
 
     companion object {
         private const val DELAY_MS = 3000L
     }
 
-    fun init(emitSuccess: Boolean) = apply {
-        this.emitSuccess = emitSuccess
-    }
-
-    override fun prepare(): Maybe<Boolean> = Maybe.create<Boolean> { emitter ->
+    override fun prepare(emitSuccess: Boolean) = Maybe.create<Boolean> { emitter ->
         if (emitSuccess) {
             emitter.onSuccess(true)
         } else {
