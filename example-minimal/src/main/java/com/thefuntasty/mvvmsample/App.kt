845a2186c05af831a38b63a120b89d2fd59cc46e
Martin Sumera <sumera.martin@gmail.com>
2019-11-17 20:05:30 +0100

    Add global error logger for interactors

--- example-minimal/src/main/java/com/thefuntasty/mvvmsample/App.kt
+++ example-minimal/src/main/java/com/thefuntasty/mvvmsample/App.kt
@@ -1,10 +1,20 @@
 package com.thefuntasty.mvvmsample
 
+import android.util.Log
+import com.thefuntasty.interactors.InteractorErrorHandler
 import com.thefuntasty.mvvmsample.injection.DaggerApplicationComponent
 import dagger.android.AndroidInjector
 import dagger.android.support.DaggerApplication
 
 class App : DaggerApplication() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        InteractorErrorHandler.globalOnErrorLogger = { error ->
+            Log.d("Interactor error", "$error")
+        }
+    }
 
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
         val component = DaggerApplicationComponent.builder().application(this).build()
