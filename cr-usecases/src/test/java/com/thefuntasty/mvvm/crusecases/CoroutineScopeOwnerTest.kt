c2aafde4864f583ed6e99e219cd383f392f77d9b
Lukas Sztefek <lukas.sztefek@gmail.com>
2019-12-16 15:30:01 +0100

    Reformat tests

--- cr-usecases/src/test/java/com/thefuntasty/mvvm/crusecases/CoroutineScopeOwnerTest.kt
+++ cr-usecases/src/test/java/com/thefuntasty/mvvm/crusecases/CoroutineScopeOwnerTest.kt
@@ -14,30 +14,31 @@ class CoroutineScopeOwnerTest : BaseCoroutineScopeOwnerTest() {
     fun previousExecutionCanceled() {
         val testUseCase = TestUseCase()
         var count = 0
+
         testUseCase.execute(1) {
             onSuccess { count++ }
-            onError {
-                Assert.fail("Exception thrown where shouldn't")
-            }
+            onError { Assert.fail("Exception thrown where shouldn't") }
         }
         coroutineScope.advanceTimeBy(500)
+
         testUseCase.execute(1) {
             onSuccess { count++ }
-            onError {
-                Assert.fail("Exception thrown where shouldn't")
-            }
+            onError { Assert.fail("Exception thrown where shouldn't") }
         }
         coroutineScope.advanceTimeBy(1000)
-        Assert.assertEquals("PreviousExecutionNotCanceled", 1, count)
+
+        Assert.assertEquals("Previous execution canceled", 1, count)
     }
 
     @Test
     fun onErrorCalled() {
         val testFailureUseCase = TestFailureUseCase()
         var resultError: Throwable? = null
+
         testFailureUseCase.execute(IllegalStateException()) {
             onError { resultError = it }
         }
+
         Assert.assertNotNull(resultError)
     }
 
@@ -46,25 +47,20 @@ class CoroutineScopeOwnerTest : BaseCoroutineScopeOwnerTest() {
         val testFlowUseCase = TestFlowUseCase()
         val testingList = listOfNotNull(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
         val resultList = mutableListOf<Int>()
+
         testFlowUseCase.execute(TestFlowUseCase.Data(testingList, 1000)) {
             onNext { resultList.add(it) }
-            onError {
-                it.printStackTrace()
-                Assert.fail("Exception thrown where shouldn't")
-            }
-            onComplete {
-                Assert.fail("onComplete called where shouldn't")
-            }
+            onError { Assert.fail("Exception thrown where shouldn't") }
+            onComplete { Assert.fail("onComplete called where shouldn't") }
         }
 
         testFlowUseCase.execute(TestFlowUseCase.Data(testingList, 1000)) {
             onNext { resultList.add(it) }
-            onError {
-                Assert.fail("Exception thrown where shouldn't")
-            }
+            onError { Assert.fail("Exception thrown where shouldn't") }
         }
         coroutineScope.advanceTimeBy(10000)
-        Assert.assertEquals("PreviousExecutionNotCanceled", testingList, resultList)
+
+        Assert.assertEquals("Previous execution canceled", testingList, resultList)
     }
 
     @Test
@@ -72,33 +68,27 @@ class CoroutineScopeOwnerTest : BaseCoroutineScopeOwnerTest() {
         val testFlowUseCase = TestFlowUseCase()
         var completed = false
         val testingList = listOfNotNull(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
+
         testFlowUseCase.execute(TestFlowUseCase.Data(testingList, 1000)) {
-            onError {
-                Assert.fail("Exception thrown where shouldn't")
-            }
-            onComplete {
-                completed = true
-            }
+            onError { Assert.fail("Exception thrown where shouldn't") }
+            onComplete { completed = true }
         }
         coroutineScope.advanceTimeBy(10000)
-        Assert.assertEquals("PreviousExecutionNotCanceled", true, completed)
+
+        Assert.assertEquals("Execution completed successfully", true, completed)
     }
 
     @Test
     fun flowOnErrorCalled() {
         val testFlowFailureUseCase = TestFailureFlowUseCase()
         var resultError: Throwable? = null
+
         testFlowFailureUseCase.execute(IllegalStateException()) {
-            onNext {
-                Assert.fail("onNext called where shouldn't")
-            }
-            onError {
-                resultError = it
-            }
-            onComplete {
-                Assert.fail("onComplete called where shouldn't")
-            }
+            onNext { Assert.fail("onNext called where shouldn't") }
+            onError { resultError = it }
+            onComplete { Assert.fail("onComplete called where shouldn't") }
         }
+
         Assert.assertNotNull(resultError)
     }
 }
