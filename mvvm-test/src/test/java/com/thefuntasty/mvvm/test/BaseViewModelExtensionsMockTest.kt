d5e40af744bcbbf3aa0ef990344655ab4a8f443b
Martin Sumera <sumera.martin@gmail.com>
2020-01-09 13:10:19 +0100

    Name refactor + lint issues

--- mvvm-test/src/test/java/com/thefuntasty/mvvm/test/BaseViewModelExtensionsMockTest.kt
+++ mvvm-test/src/test/java/com/thefuntasty/mvvm/test/BaseViewModelExtensionsMockTest.kt
@@ -31,7 +31,7 @@ class BaseViewModelExtensionsMockTest : ViewModelTest() {
     @Test
     fun `everyObserveWithoutOwner should call captured lambda`() = with(viewModel) {
         // GIVEN
-        val capturedLambda = viewModel.everyObserveWithoutOwner { viewModel.viewState.mutableLiveData }
+        val capturedLambda = viewModel.mockObserveWithoutOwner { viewModel.viewState.mutableLiveData }
 
         // WHEN
         var result = 0
@@ -46,7 +46,7 @@ class BaseViewModelExtensionsMockTest : ViewModelTest() {
     @Test
     fun `everyObserveWithoutOwnerDefaultValue should call captured lambda`() = with(viewModel) {
         // GIVEN
-        val capturedLambda = everyObserveWithoutOwnerDefaultValue { viewModel.viewState.defaultValueLiveData }
+        val capturedLambda = mockObserveWithoutOwnerDefaultValue { viewModel.viewState.defaultValueLiveData }
 
         // WHEN
         var result = 0
@@ -61,7 +61,7 @@ class BaseViewModelExtensionsMockTest : ViewModelTest() {
     @Test
     fun `everyObserveWithoutOwnerDefaultValueMediator should call captured lambda`() = with(viewModel) {
         // GIVEN
-        val capturedLambda = everyObserveWithoutOwnerDefaultValueMediator { viewModel.viewState.defaultValueMediatorLiveData }
+        val capturedLambda = mockObserveWithoutOwnerDefaultValueMediator { viewModel.viewState.defaultValueMediatorLiveData }
 
         // WHEN
         var result = 0
