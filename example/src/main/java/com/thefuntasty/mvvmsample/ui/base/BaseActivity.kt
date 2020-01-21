45ba1e54ef5b7f16e2d7b64d08e1b677a4eac5a6
Lukas Sztefek <lukas.sztefek@gmail.com>
2020-01-21 17:20:31 +0100

    Use BR class from different package

--- example/src/main/java/com/thefuntasty/mvvmsample/ui/base/BaseActivity.kt
+++ example/src/main/java/com/thefuntasty/mvvmsample/ui/base/BaseActivity.kt
@@ -1,10 +1,10 @@
 package com.thefuntasty.mvvmsample.ui.base
 
 import androidx.databinding.ViewDataBinding
-import com.thefuntasty.mvvm.BR
 import com.thefuntasty.mvvm.BaseViewModel
 import com.thefuntasty.mvvm.ViewState
 import com.thefuntasty.mvvm.dagger.activity.BaseDaggerBindingActivity
+import com.thefuntasty.mvvmsample.BR
 
 abstract class BaseActivity<VM : BaseViewModel<VS>, VS : ViewState, B : ViewDataBinding> :
     BaseDaggerBindingActivity<VM, VS, B>() {
