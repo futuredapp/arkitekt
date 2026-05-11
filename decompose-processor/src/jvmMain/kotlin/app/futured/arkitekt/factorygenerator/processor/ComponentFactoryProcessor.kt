877eef2fcc15fc3a9356500524bc218ca65aaf13
matejsemancik <matej.semancik@gmail.com>
2026-05-11 20:00:46 +0200

    Ensure ComponentFactoryProcessor runs only once

--- decompose-processor/src/jvmMain/kotlin/app/futured/arkitekt/factorygenerator/processor/ComponentFactoryProcessor.kt
+++ decompose-processor/src/jvmMain/kotlin/app/futured/arkitekt/factorygenerator/processor/ComponentFactoryProcessor.kt
@@ -11,7 +11,15 @@ import kotlin.reflect.KClass
 class ComponentFactoryProcessor(
     private val codeGenerator: CodeGenerator,
 ) : SymbolProcessor {
+
+    var invoked: Boolean = false
+
     override fun process(resolver: Resolver): List<KSAnnotated> {
+        if (invoked) {
+            return emptyList()
+        }
+        invoked = true
+
         val components: Sequence<KSClassDeclaration> = resolver.findAnnotationsForClass(GenerateFactory::class)
         components.forEach { generateComponent(it) }
         return emptyList()
